8c65423d4151be5ba99cdbf5a648b7c8e7e2f30a
vzdzt <aproca24@gmail.com>
2025-09-28 12:40:08 -0400

    Fix static file serving path in server.js

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // yt-dlp path (global installation on VPS)
 const ytDlpPath = '/usr/local/bin/yt-dlp';
